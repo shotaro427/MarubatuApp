895f20558fa6d016c59ee686518915219b258ee5
shotaro427 <toshinobu.s111@gmail.com>
2019-08-04 10:50:09 +0900

    回答のチェックする機能の追加

--- MarubatuApp/ViewController.swift
+++ MarubatuApp/ViewController.swift
@@ -10,14 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    // クイズの問題を表示
+    /// クイズの問題を表示
     @IBOutlet weak var questionLabel: UILabel!
     
-    // 現在のクイズ番号を管理
+    /// 現在のクイズ番号を管理
     var currentQuestionNum: Int = 0
     
-    // 問題を管理 (true = マル、 false = バツ）
+    /// 問題を管理 (true = マル、 false = バツ）
     let questions: [[String: Any]] = [
+        // question = 問題、　answer = 答え
         [ "question": "iPhoneアプリを開発する統合環境はZcodeである",
           "answer": false
         ],
@@ -42,7 +43,7 @@ class ViewController: UIViewController {
         showQuestion()
     }
     
-    // 問題を表示する関数
+    /// 問題を表示する関数
     func showQuestion() {
         // currentQuestionNum(問題番号)の問題を取得
         let question = questions[currentQuestionNum]
@@ -53,15 +54,62 @@ class ViewController: UIViewController {
             questionLabel.text = que
         }
     }
+    
+    /// 回答をチェックする関数
+    /// 正解なら次の問題を表示します
+    func checkAnswer(yourAnswer: Bool) {
+        // どの問題か取得する
+        let question = questions[currentQuestionNum]
+        
+        // 問題の答えを取り出す
+        if let ans = question["answer"] as? Bool {
+            
+            // 選択された答えと問題の答えを比較する
+            if yourAnswer == ans {
+                // 正解
+                // currentQuestionNumを1足して次の問題に進む
+                currentQuestionNum += 1
+                showAlert(message: "正解!")
+            } else {
+                // 不正解
+                showAlert(message: "不正解...")
+            }
+        } else {
+            print("答えが入っていません")
+            return
+        }
+        
+        // currentQuestionNumの値が問題数以上だったら最初の問題に戻す
+        if currentQuestionNum >= questions.count {
+            currentQuestionNum = 0
+        }
+        
+        // 問題を表示します
+        // 正解であれば次の問題が、不正解であれば同じ問題が再表示されます
+        showQuestion()
+    }
+    
+    /// アラートを表示する関数
+    func showAlert(message: String) {
+        // アラートの作成
+        let alert = UIAlertController(title: nil, message: message, preferredStyle:  .alert)
+        // アラートのアクション（ボタン部分の定義）
+        let close = UIAlertAction(title: "閉じる", style: .cancel, handler: nil)
+        // 作成したalertに閉じるボタンを追加
+        alert.addAction(close)
+        // アラートを表示する
+        present(alert, animated: true, completion: nil)
+    }
 
     // バツボタン
     @IBAction func tappedNoButton(_ sender: UIButton) {
+        checkAnswer(yourAnswer: false)
     }
     
     // マルボタン
     @IBAction func tappedYesButton(_ sender: UIButton) {
+        checkAnswer(yourAnswer: true)
     }
-    
     
 }
 
